30221fadc9fbe2a07c8620fd3110ce3781e8d7fd
Fredrik Atmer <fredrik@atmer.se>
2024-12-17 11:24:34 +0100

    Updates to solution for December 16th.
    Don't put in duplicate edges for undirected graph, and
    nx.all_shortest_paths() work like supposed to.

--- 16/solution.py
+++ 16/solution.py
@@ -27,7 +27,7 @@ def parse_input(file_handle) -> tuple[nx.Graph, Node, Node]:
     for p in __maze:
         for d1,d2 in zip(directions, (1j*d for d in directions)):
             maze.add_edge(Node(p,d1), Node(p,d2), weight=1000)
-        for d in directions:
+        for d in directions[:2]:
             if p+d in __maze:
                 maze.add_edge(Node(p,d), Node(p+d,d), weight=1)
     maze.add_edge(source, Node(start,1), weight=0)
@@ -38,21 +38,12 @@ def parse_input(file_handle) -> tuple[nx.Graph, Node, Node]:
 
 def part1(problem_input:tuple[nx.Graph, Node, Node]) -> int:
     maze,source,sink = problem_input
-    return nx.dijkstra_path_length(maze, source, sink)
+    return nx.shortest_path_length(maze, source, sink, weight='weight')
 
 
 def part2(problem_input:tuple[nx.Graph, Node, Node]) -> int:
     maze,source,sink = problem_input
-    best_score = part1(problem_input)
-    distances:dict[Node,dict[Node,int]] = {
-        source: dict(nx.single_source_dijkstra_path_length(maze, source, cutoff=best_score)),
-        sink: dict(nx.single_source_dijkstra_path_length(maze, sink, cutoff=best_score)),
-    }
-    nodes_to_check = distances[source].keys() & distances[sink].keys() - {source, sink}
-    good_nodes:set[Node] = set()
-    for node in nodes_to_check:
-        if distances[source][node] + distances[sink][node] <= distances[source][sink]:
-            good_nodes.add(node)
-    good_nodes -= {source, sink}
-    good_tiles:set[complex] = {n.position for n in good_nodes}
+    good_tiles:set[complex] = set()
+    for path in nx.all_shortest_paths(maze, source, sink, weight="weight"):
+        good_tiles |= {n.position for n in path[1:-1]}
     return len(good_tiles)
