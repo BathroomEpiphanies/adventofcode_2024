bc82a26243b0d8ba2bde4db7aa7edb9923cbe21f
Fredrik Atmer <fredrik@atmer.se>
2024-12-19 14:39:06 +0100

    Performance improvements to solution for December 19th.

--- 19/solution.py
+++ 19/solution.py
@@ -7,29 +7,30 @@ def parse_input(file_handle) -> tuple[list[str],list[str]]:
     lines = (l.strip() for l in file_handle.readlines())
     towels = next(lines).split(', ')
     next(lines)
-    patterns = [l for l  in lines]
-    return towels,patterns
+    designs = [l for l  in lines]
+    return towels,designs
 
 
-def create_towel_finder(towels:list[str]) -> Callable[[str,str], int]:
+def create_design_finder(towels:list[str]) -> Callable[[str], int]:
     @cache
-    def __fun(pattern:str, towel:str='') -> int:
-        if pattern == towel:
+    def __function(design:str) -> int:
+        if design == '':
             return 1
-        elif pattern.startswith(towel):
-            return sum(__fun(pattern.removeprefix(towel), t) for t in towels)
         else:
-            return 0
-    return __fun
+            return sum(
+                __function(design.removeprefix(t))
+                for t in towels if design.startswith(t)
+            )
+    return __function
 
 
 def part1(problem_input:tuple[list[str],list[str]]) -> int:
-    towels,patterns = problem_input
-    towel_pattern_finder = create_towel_finder(towels)
-    return ilen(p for p in patterns if towel_pattern_finder(p, '')>0)
+    towels,designs = problem_input
+    towel_pattern_finder = create_design_finder(towels)
+    return ilen(p for p in designs if towel_pattern_finder(p)>0)
 
 
 def part2(problem_input:tuple[list[str],list[str]]) -> int:
-    towels,patterns = problem_input
-    towel_pattern_finder = create_towel_finder(towels)
-    return sum(towel_pattern_finder(p, '') for p in patterns)
+    towels,designs = problem_input
+    towel_pattern_finder = create_design_finder(towels)
+    return sum(towel_pattern_finder(p) for p in designs)
